65e205d6fd6fcb59eae9755eaae318d92cbce5ae
tianming <tianming@qunhemail.com>
2018-07-21 22:43:03 +0800

    add annotation

--- src/main/java/com/lovevol/mysite/config/WebAppConfig.java
+++ src/main/java/com/lovevol/mysite/config/WebAppConfig.java
@@ -1,15 +1,19 @@
 package com.lovevol.mysite.config;
 
 import com.lovevol.mysite.util.Interceptor.AuthInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebAppConfig extends WebMvcConfigurationSupport{
 
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/**").excludePathPatterns("/user/login");
+        registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns("/user/login")
+                .excludePathPatterns("/static/**");
     }
 }
