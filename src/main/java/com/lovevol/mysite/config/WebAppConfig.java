2e38b0473bfee637cd7451ea96ad1cd4e504f5d0
tianming <tianming@qunhemail.com>
2018-07-30 15:21:06 +0800

    init

--- src/main/java/com/lovevol/mysite/config/WebAppConfig.java
+++ src/main/java/com/lovevol/mysite/config/WebAppConfig.java
@@ -1,11 +1,17 @@
 package com.lovevol.mysite.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.lovevol.mysite.util.Interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Configuration
 public class WebAppConfig extends WebMvcConfigurationSupport{
@@ -31,5 +37,15 @@ public class WebAppConfig extends WebMvcConfigurationSupport{
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        converter.setFastJsonConfig(config);
+        converters.add(converter);
+
     }
 }
