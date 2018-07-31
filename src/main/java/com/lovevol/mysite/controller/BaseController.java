8379163687ebc841d13e15d28a9f2650488c7129
tianming <tianming@qunhemail.com>
2018-07-31 09:30:14 +0800

    init

--- src/main/java/com/lovevol/mysite/controller/BaseController.java
+++ src/main/java/com/lovevol/mysite/controller/BaseController.java
@@ -2,10 +2,13 @@ package com.lovevol.mysite.controller;
 
 import com.github.pagehelper.PageHelper;
 import com.lovevol.mysite.util.PageParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author lh
  */
+@Controller
 public class BaseController {
     public void setPageParam(PageParam pageParam){
         PageHelper.startPage(pageParam.getPageNum(), pageParam.getPageSize());
@@ -13,5 +16,10 @@ public class BaseController {
 
     public void setPageParam(Integer pageNum, Integer pageSize){
         PageHelper.startPage(pageNum, pageSize);
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/admin/goIndex";
     }
 }
